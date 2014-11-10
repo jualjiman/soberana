3e7e96817ed7e2c32feff2f55f9b764cb55b2f56
Juan Alberto Jimenez Angel <jualjiman@gmail.com>
2014-11-10 16:38:05 -0600

    youtube video de publicacion fix

--- fabfile.py
+++ fabfile.py
@@ -1,32 +1,134 @@
-rom fabric.api import  *
-from fabric.colors import green
+from fabric.api import  *
+from fabric.colors import green,red
+from fabvenv import virtualenv
 import cuisine
 
-env.app_dir = 'ford-experience.stg.interalia.net/ford-experience'
+env.webapps_dir = '/webapps/'
+env.app_dir = '/webapps/ITA/site'
+env.env_dir = '/webapps/ITA/ita-env'
+
 env.roledefs = {
-    'avaricia': ['web@10.0.0.7'],
+    'hitman': ['root@191.101.13.47'],
 } 
 
 @task
-def deploy(branch="develop"):
+def supervisor_restart():
+    """
+        restart project with supervisor
+    """
+    print red("---SUPERVISOR RESTART---")
+    sudo("supervisorctl restart ITA")
+    print green("Restarted")
+
+@task
+def supervisor_stop():
+    """
+        stop project with supervisor
+    """
+    print red("---SUPERVISOR STOP---")
+    sudo("supervisorctl stop ITA")
+    print green("Stoped")
+
+@task
+def supervisor_status():
+    """
+        get project supervisor status
+    """
+    print red("---SUPERVISOR STATUS---")
+    sudo("supervisorctl status ITA")
+    print green("Done")
+
+
+############################################################################################
+
+# fab deploy:<branch> -R hitman
+@task
+def git_pull(branch="master"):
     """
         funcion para actualizar el branch y reiniciar el proyecto
     """
+    print red("---GIT PULL---")
     with cd(env.app_dir):
         print green("actualizando...")
         run("git pull origin %s" % branch)
 
-        print green("recargando wsgi.py...")
-        run("touch facebook_pro/facebook_pro/wsgi.py")
+        supervisor_restart()
 
         print green("Listo")
 
 @task
-def install_pip_dependences():
-    print green("INSTALLING PIP DEPENDENCES")
-    print green("changing env")
-    run("source ../ita-env/bin/activate")
-    print green("installing")
-    run("pip install -r requirements.txt")
+def git_push(branch="master"):
+    """
+        funcion para hacer push de los cambios del proyecto    
+    """
+    print red("---GIT PUSH---")
+    with cd(env.app_dir):
+        print green("actualizando...")
+        run("pip freeze > requeriments.txt")
+        run("git status")
+        run("git add -A")
+        run("git commit -m 'subiendo cambios server'")
+        run("git push origin %s" % branch)
+
+        print green("Listo")
+
+############################################################################################
+
+#fab create -R hitman
+@task
+def create():
+    """
+        creates files, download dependences and download master's branch project
+    """
+    with cd(env.webapps_dir):
+        print red("---CREATE PROJECT---")
+
+        print green("---creating root file---")
+        run("mkdir ITA")
+
+        print green("---creating project file and virtualenv---")
+        with cd("ITA"):
+            run("mkdir logs/")
+            run("mkdir run/")
+            run("touch logs/gunicorn_supervisor.log")
+            run("mkdir site")
+            run("virtualenv ita-env")
+
+            print green("---initializing git, adding remote and downloading master---")    
+            with cd("site"):
+                run("git init")
+                run("git remote add origin git@github.com:jualjiman/soberana.git")
+                run("git pull origin master")
+
+            print green("---installing pip requeriments---")
+            run("source ita-env/bin/activate && pip install -r site/requeriments.txt")
 
 
+            
+
+
+###########################################################################################
+
+@task
+def django_syncdb():
+    """
+        funcion para hacer push de los cambios del proyecto    
+    """
+    print red("---DJANGO SYNCDB---")
+    with cd(env.app_dir):
+        print green("actualizando...")
+        run("source ../ita-env/bin/activate && ./manage.py syncdb")
+
+        print green("Listo")
+
+@task
+def pip_install(package="master"):
+    """
+        instalar un paquete con pip
+    """
+    print red("---PIP INSTALL---")
+    with cd(env.env_dir):
+        print green("installing...")
+        run("source bin/activate && pip install %s" % package)
+
+        print green("Listo")
