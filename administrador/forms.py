96d5e5dfb1d3a6e27c41978d7a1b14d8cab9ad3e
Juan Alberto Jimenez Angel <jualjiman@gmail.com>
2014-11-10 17:51:19 -0600

    agregando buscador

---
+++ administrador/forms.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+from django import forms
+
+class BusquedaForm(forms.Form):
+	pista = forms.CharField(max_length=100,widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'¿Qué evento esta buscando?'}))
--- administrador/views.py
+++ administrador/views.py
@@ -3,6 +3,7 @@ from django.db.models import Q
 from django.shortcuts import render, get_object_or_404
 from datetime import datetime
 from .models import *
+from .forms import *
 
 # Create your views here.
 
@@ -29,6 +30,81 @@ def home(request):
 				"titulo" : titulo
 			}
 		)
+
+def publicacion(request, ide):
+	publicacion = get_object_or_404(Publicacion,activo = True, id=ide)
+
+	now = datetime.now()
+	otrasPublicaciones = Publicacion.objects.filter(
+		Q(fecha_inicio__lte=now), 
+		Q(fecha_fin__gte=now) | Q(fecha_fin__isnull=True), 
+		Q(activo = True)).exclude(pk=ide).order_by("-categoria")[:4]
+
+	titulo = "%s - " % (publicacion.titulo,)
+	return render(
+		request,
+		"publicacion.html",
+		{
+			"publicacion" : publicacion,
+			"titulo" : titulo,
+			"otrasPublicaciones" : otrasPublicaciones
+		}
+	)
+
+def publicaciones(request):
+	titulo = "Publicaciones - "
+	now = datetime.now()
+	todasPublicaciones = Publicacion.objects.filter(
+		Q(fecha_inicio__lte=now), 
+		Q(fecha_fin__gte=now) | Q(fecha_fin__isnull=True), 
+		Q(activo = True)).order_by("-categoria")
+
+	publicacionesPermanentes = todasPublicaciones[:4]
+	publicaciones = todasPublicaciones[4:8]
+
+	searchform = BusquedaForm()
+
+	return render(
+		request,
+		"publicaciones.html",
+		{
+			"publicacionesPermanentes" : publicacionesPermanentes,
+			"publicaciones" : publicaciones,
+			"titulo" : titulo,
+			"searchform" : searchform
+		}
+	)
+
+def busqueda(request):
+	if request.method == 'GET': # If the form has been submitted...
+        # ContactForm BusquedaForm was defined in the previous section
+        #FUNCIONA PERFECTAMENTE POR GET O POST
+		searchform = BusquedaForm(request.GET) # A form bound to the POST data
+		if searchform.is_valid(): # All validation rules pass
+	        # Process the data in form.cleaned_data
+	        # ...
+			pista = searchform.cleaned_data['pista']
+			if pista != u'':
+				publicaciones = Publicacion.objects.filter(
+					Q(fecha_inicio__lte=now), 
+					Q(fecha_fin__gte=now) | Q(fecha_fin__isnull=True), 
+					Q(activo = True),
+					Q(titulo__contains=pista) | Q(resumen__contains=pista) | Q(texto__contains=pista)
+					).order_by("-categoria")
+
+				textoBusqueda = 'Resultados para "' + pista + '"'
+				return render(request, 'busqueda.html', {
+					"publicaciones": publicaciones,
+					"textoBusqueda":textoBusqueda
+					})
+			else:
+				return HttpResponseRedirect("/")
+		else:
+				return HttpResponseRedirect("/")
+	else:
+		return HttpResponseRedirect("/")
+
+################################################################################################
 
 def directorio(request):
 	titulo = "Directorio - "
@@ -89,44 +165,3 @@ def postgrados(request):
 				"titulo" : titulo,
 			}
 		)
-
-def publicacion(request, ide):
-	publicacion = get_object_or_404(Publicacion,activo = True, id=ide)
-
-	now = datetime.now()
-	otrasPublicaciones = Publicacion.objects.filter(
-		Q(fecha_inicio__lte=now), 
-		Q(fecha_fin__gte=now) | Q(fecha_fin__isnull=True), 
-		Q(activo = True)).exclude(pk=ide).order_by("-categoria")[:4]
-
-	titulo = "%s - " % (publicacion.titulo,)
-	return render(
-		request,
-		"publicacion.html",
-		{
-			"publicacion" : publicacion,
-			"titulo" : titulo,
-			"otrasPublicaciones" : otrasPublicaciones
-		}
-	)
-
-def publicaciones(request):
-	titulo = "Publicaciones - "
-	now = datetime.now()
-	todasPublicaciones = Publicacion.objects.filter(
-		Q(fecha_inicio__lte=now), 
-		Q(fecha_fin__gte=now) | Q(fecha_fin__isnull=True), 
-		Q(activo = True)).order_by("-categoria")
-
-	publicacionesPermanentes = todasPublicaciones[:4]
-	publicaciones = todasPublicaciones[4:8]
-
-	return render(
-		request,
-		"publicaciones.html",
-		{
-			"publicacionesPermanentes" : publicacionesPermanentes,
-			"publicaciones" : publicaciones,
-			"titulo" : titulo
-		}
-	)
