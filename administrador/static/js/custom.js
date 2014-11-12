8535ecef842aa49fe30bcdb60eb3d54109970872
Juan Alberto Jimenez Angel <jualjiman@gmail.com>
2014-11-12 11:18:34 -0600

    aplicando el boton cargar mas

--- administrador/static/js/custom.js
+++ administrador/static/js/custom.js
@@ -8,43 +8,7 @@ scripts.sliderOnResize = function (){
 	}
 };
 
-// Load more
-   $('#btn-loadmore').on( 'click', function(e) {
-      e.preventDefault();
-      //var $this = $(this).hide();
-      //var $spinner = $(this).siblings('.loading-spinner').show();
-      var pag = parseInt($("#num-pag").val());
 
-      var $gallery = $("#loadmore-container");
-      console.log($gallery);
-      // $.ajax({
-      //    type: "POST",
-      //    url: '/mas/',
-      //    data: {
-      //       num: pag
-      //    },
-      //    success: function (data) {
-      //       $("#num-pag").val(pag + 1);
-      //       // Get elements from request
-      //       var $elems = $('<div>'+data+'</div>').find('.gl-item');
-      //       // append elements to container
-      //       $gallery.append( $elems );
-      //          // add and lay out newly appended elements
-      //          refresh_items_order_data();
-      //       $gallery.isotope( 'appended', $elems );
-               
-      //       process_images_load();
-            
-      //    },
-      //    complete: function () {
-      //       $spinner.hide();
-      //    },
-      //    error: function (jqXHR, textStatus) {
-      //       console.log(textStatus);
-      //    }
-      // });
-
-   });
 
 $( window ).resize(function() {
 	scripts.sliderOnResize();
@@ -52,4 +16,43 @@ $( window ).resize(function() {
 
 $(function(){
 	scripts.sliderOnResize();
+
+	// Load more
+	$('#btn-loadmore').click(function(e) {
+	  e.preventDefault();
+	  //var $this = $(this).hide();
+	  //var $spinner = $(this).siblings('.loading-spinner').show();
+	  //var pag = parseInt($("#num-pag").val());
+
+	  var $gallery = $("#loadmore-container");
+	  console.log($gallery);
+	  alert("ya");
+	  // $.ajax({
+	  //    type: "POST",
+	  //    url: '/mas/',
+	  //    data: {
+	  //       num: pag
+	  //    },
+	  //    success: function (data) {
+	  //       $("#num-pag").val(pag + 1);
+	  //       // Get elements from request
+	  //       var $elems = $('<div>'+data+'</div>').find('.gl-item');
+	  //       // append elements to container
+	  //       $gallery.append( $elems );
+	  //          // add and lay out newly appended elements
+	  //          refresh_items_order_data();
+	  //       $gallery.isotope( 'appended', $elems );
+	           
+	  //       process_images_load();
+	        
+	  //    },
+	  //    complete: function () {
+	  //       $spinner.hide();
+	  //    },
+	  //    error: function (jqXHR, textStatus) {
+	  //       console.log(textStatus);
+	  //    }
+	  // });
+
+	});
 });
