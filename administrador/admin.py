797cab75682cbdc8d4c9f9e840f2ef32d005844f
Juan Alberto Jimenez Angel <jualjiman@gmail.com>
2015-02-10 13:05:18 -0600

    mostrando solamente campos necesarios de usuario

--- administrador/admin.py
+++ administrador/admin.py
@@ -3,7 +3,48 @@ from .models import *
 from sorl.thumbnail.shortcuts import get_thumbnail
 from django.contrib.auth.models import User
 
+from django.contrib.auth.admin import UserAdmin
+
 # Register your models here.
+
+#UserAdmin.list_display = ('username', 'email', 'first_name', 'last_name','get_group_permissions')
+
+
+class MyUserAdmin(UserAdmin):
+	list_display = ('username', 'email', 'first_name', 'last_name', 'grupos')
+
+	def save_model(self, request, obj, form, change):
+		obj.is_staff = True
+		obj.save()
+
+	## Static overriding 
+	fieldsets = (
+	    (None, {'fields': ('username', 'password')}),
+	    (('Personal info'), {'fields': ('first_name', 'last_name', 'email')}),
+	    (('Permissions'), {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups')}),
+	    #(('Important dates'), {'fields': ('last_login', 'date_joined')}),
+	)
+
+	def get_form(self, request, obj=None, **kwargs):
+	    self.exclude = ("user_permissions",)
+	    ## Dynamically overriding
+	    self.fieldsets[2][1]["fields"] = ('is_superuser','groups',)
+	    
+	    form = super(MyUserAdmin,self).get_form(request, obj, **kwargs)
+	    return form
+	def grupos(self,model_instance):
+		grupos = model_instance.groups.all()
+		string_grupos = ""
+		
+		for nombre in grupos:
+			string_grupos += str(nombre) + " "
+
+		return string_grupos
+
+
+
+admin.site.unregister(User)
+admin.site.register(User, MyUserAdmin)
 
 class EnlaceInline(admin.StackedInline):
 	model = EnlacePublicacion
--- administrador/models.py
+++ administrador/models.py
@@ -29,17 +29,8 @@ class Publicacion(models.Model):
 	imagen = ImageField(upload_to = "publicaciones")
 	titulo = models.CharField(max_length=60, help_text='Titulo de la publicacion')
 	resumen = models.CharField(max_length=150, help_text='Reseña de la publicacion que sera mostrada (150 caracteres)')
-	#texto = models.TextField(help_text='Texto correspondiente a la publicacion')
+
 	texto = RichTextField()
-	
-	# descripcionLink = models.CharField(max_length=60, blank=True, help_text="Texto descriptivo del link.")
-	# link = models.URLField(blank=True, help_text='Link asociado con la publicacion')
-	
-	# descripcionVideoYoutube = models.CharField(max_length=60, blank=True, help_text="Texto descriptivo del video de youtube.")
-	# claveYoutube = models.CharField(max_length=20, blank=True, help_text='Clave de video de youtube asociado con la publicacion')
-	
-	# descripcionPdf = models.CharField(max_length=60, blank=True, help_text="Texto descriptivo del PDF.")
-	# pdf = models.FileField(upload_to = "pdfpublicacion", blank=True, help_text='Archivo pdf asociado con la publicacion')
 	
 	fecha = models.DateField(default=datetime.now(),help_text='Fecha a partir de la cual sera mostrada la publicacion')
 	fecha_inicio = models.DateTimeField(default=datetime.now(), help_text='Fecha y hora de inicio de la publicacion')
