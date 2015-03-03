908f0851120690027fa0e1fcdae90c143922f37d
Juan Alberto Jimenez Angel <jualjiman@gmail.com>
2015-03-03 10:38:50 -0600

    correccion de mensaje de director

--- administrador/static/js/custom.js
+++ administrador/static/js/custom.js
@@ -1,50 +1,50 @@
 var scripts = {};
 
-// scripts.sliderOnResize = function (){
-// 	if($(window).width() > 1500){
-// 		$("#slider").addClass("container");
-// 	}else{
-// 		$("#slider").removeClass("container");
-// 	}
-// };
+scripts.sliderOnResize = function (){
+	if($(window).width() > 1920){
+		$("#slider").addClass("container");
+	}else{
+		$("#slider").removeClass("container");
+	}
+};
 
 
 
-// $( window ).resize(function() {
-// 	scripts.sliderOnResize();
-// });
+$( window ).resize(function() {
+	scripts.sliderOnResize();
+});
 
-// $(function(){
-// 	scripts.sliderOnResize();
+$(function(){
+	scripts.sliderOnResize();
 
-// 	// Load more
-// 	$('#btn-loadmore').click(function(e) {
-// 	  e.preventDefault();
-// 	  //var $this = $(this).hide();
-// 	  //var $spinner = $(this).siblings('.loading-spinner').show();
-// 	  var $counter = $("#num-pag");
-// 	  var $gallery = $("#loadmore-container");
+	// Load more
+	$('#btn-loadmore').click(function(e) {
+	  e.preventDefault();
+	  //var $this = $(this).hide();
+	  //var $spinner = $(this).siblings('.loading-spinner').show();
+	  var $counter = $("#num-pag");
+	  var $gallery = $("#loadmore-container");
 	  
-// 	  var pag = parseInt($counter.val());
-// 	  $.ajax({
-// 	     type: "POST",
-// 	     url: '/mas/',
-// 	     data: {
-// 	        num: pag
-// 	     },
-// 	     success: function (data) {
-// 	        $counter.val(pag + 1);
+	  var pag = parseInt($counter.val());
+	  $.ajax({
+	     type: "POST",
+	     url: '/mas/',
+	     data: {
+	        num: pag
+	     },
+	     success: function (data) {
+	        $counter.val(pag + 1);
 
-// 	        // Get elements from request
-// 	        var $elems = $(data);
-// 	        $gallery.append( $elems.hide().fadeIn(500) );	        
-// 	     },
-// 	     error: function (jqXHR, textStatus) {
-// 	        console.log(textStatus);
-// 	     }
-// 	  });
+	        // Get elements from request
+	        var $elems = $(data);
+	        $gallery.append( $elems.hide().fadeIn(500) );	        
+	     },
+	     error: function (jqXHR, textStatus) {
+	        console.log(textStatus);
+	     }
+	  });
 
-// 	});
+	});
 
-// 	initialize();
-// });
+	initialize();
+});
